279d5b919a69c4223462d5f705243c703425e02e
Мирослав Здравковић <miroslav.zdravkovic@gmail.com>
2017-01-19 14:26:40 +0100

    Getting All Users (GET/api/users)

--- server.js
+++ server.js
@@ -87,6 +87,16 @@ apiRouter.route('/users')
 
             res.json({ message: 'User created!' });
         });
+    })
+
+    // get all the users (accessed at GET http://localhost:8080/api/users)
+    .get(function(req, res) {
+        User.find(function(err, users) {
+            if (err) res.send(err);
+
+            // return the users
+            res.json(users);
+        });
     });
 
 // REGISTER OUR ROUTES --------------------
